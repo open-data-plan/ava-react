4cd9f2ba3173521fbc1296d82ba8e2cc795e77a4
kagawagao <kingsongao1221@gmail.com>
2020-12-21 11:34:04 +0800

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: '@opd/jest-preset-pangu',
   setupFiles: ['jest-canvas-mock'],
+  transformIgnorePatterns: ['/node_modules/', '/src/.umi/'],
+  collectCoverageFrom: [
+    './src/**/*.{ts,tsx}',
+    '!./**/*.d.ts',
+    '!src/.umi/**/*.ts',
+  ],
+  coverageReporters: ['json', 'text', 'lcov', 'clover', 'cobertura'],
 }
